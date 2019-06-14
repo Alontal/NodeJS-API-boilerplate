b3a0f18599f63cac18b59c0691586f36328a4bda
omer <omer2500@gmail.com>
2019-06-14 21:13:13 +0300

    changed order to make everything load properly

--- app/util/index.js
+++ app/util/index.js
@@ -1,6 +1,6 @@
+module.exports.dateFormats = require('./dateFormats');
 module.exports.logger = require('./logger');
 module.exports.encryption = require('./encryption');
-module.exports.dateFrmats  = require('./dateFormats');
-module.exports.errorHandle =  require('./errorHandle');
-module.exports.env =  require('./env');
-module.exports.responseHandler =  require('./responseHandler');
+module.exports.errorHandle = require('./errorHandle');
+module.exports.env = require('./env');
+module.exports.responseHandler = require('./responseHandler');
