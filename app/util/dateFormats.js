26831c85274e11ac87e71bb482eb89cde432db19
omer <omer2500@gmail.com>
2019-06-14 21:10:31 +0300

    added format to match the best practices

--- app/util/dateFormats.js
+++ app/util/dateFormats.js
@@ -1,4 +1,5 @@
 module.exports = {
 	FULLDATETIME: 'dddd, MMMM Do YYYY, HH:mm:ss',
-	SHORTDATETIME: 'DD MM YYYY, HH:mm:ss'
+	SHORTDATETIME: 'DD MM YYYY, HH:mm:ss',
+	REVERSED_SHORTDATE_TIME: 'YYYY-MM-DD HH:mm:ss'
 };
