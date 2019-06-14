56b21203b525c1ec25a49a1d1c0076fb5e1a50c8
omer <omer2500@gmail.com>
2019-06-14 21:09:20 +0300

    refactor logger to match winston new version

--- app/util/logger.js
+++ app/util/logger.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-console */
-const winston = require('winston'),
+const { createLogger, format, transports } = require('winston'),
 	path = require('path'),
 	fs = require('fs'),
 	mkdirp = require('mkdirp'),
-	moment = require('moment');
+	{ dateFormats } = require('./');
+
+require('winston-daily-rotate-file');
 
 const logs_config = {
 	log_file_name: process.env.APP_DOMAIN || 'APP_DOMAIN',
@@ -18,61 +19,71 @@ const logs_config = {
 
 const LOG_DIR = path.resolve(__dirname, '..', '..', 'LOG/');
 if (!fs.existsSync(LOG_DIR)) {
-	mkdirp(LOG_DIR, (err) => {
+	mkdirp(LOG_DIR, err => {
 		if (err) console.error(err);
 		else console.log('log directory created!');
 	});
 }
 
-console.info('Logs will write to Directory >> ', LOG_DIR);
-	
-const timeStampFormat = () => moment().format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
-
 //daily rotate log files
-require('winston-daily-rotate-file');
-const daily_rotate_transport = new(winston.transports.DailyRotateFile)({
-	filename: `${LOG_DIR}/ ${logs_config.log_file_name}.log`,
+const daily_rotate_transport = level => ({
+	filename:
+		level !== 'error'
+			? `${LOG_DIR}/ ${logs_config.log_file_name}.log`
+			: `${LOG_DIR}/ ${logs_config.log_file_name}-error.log`,
 	prepend: true,
-	// level: process.env.ENV === 'development' ? 'debug' : 'info'
-	level: logs_config.level_rotate,
+	level: level,
 	maxSize: '20m',
-	maxFiles: '14d',
-	json: true,
-	timestamp: timeStampFormat
+	maxFiles: '14d'
 });
 
-//log error messages to console and everything rate as info and below save to file (which includes error messages):
-const console_opts = { //set the level to print to console 
-	level: logs_config.level_console,
-	colorize: true,
-	prettyPrint: true,
-	timestamp: timeStampFormat,
-	json: false
-	// logstash: true
-};
+console.info('Logs will write to Directory >> ', LOG_DIR);
 
-//set winston transports to obj's define above
-var logger = new winston.Logger({
-	levels: winston.config.syslog.levels,
+const logger = createLogger({
+	level: 'info',
+	format: format.combine(
+		format.timestamp({
+			format: dateFormats.REVERSED_SHORTDATE_TIME
+		}),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.json()
+	),
 	transports: [
-		new winston.transports.Console(console_opts),
-		// new winston.transports.File(file_opts),
-		daily_rotate_transport
-
+		//
+		// - Write to all logs with level `info` and below to `combined.log`
+		// - Write all logs error (and below) to `error.log`.
+		//
+		new transports.DailyRotateFile(daily_rotate_transport('error')),
+		new transports.DailyRotateFile(daily_rotate_transport('info'))
 	]
 });
-// make winston catch Exceptions and print them to diff log file
-if (process.env.NODE_ENV === 'production') {
-	logger.handleExceptions(
-		new winston.transports.File({
-			filename: LOG_DIR + logs_config.log_file_name_Exceptions
-		}));
-	//prevent winston to stop if Error occurred  
+
+//
+// If we're not in production then **ALSO** log to the `console`
+// with the colorized simple format.
+//
+
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(
+		new transports.Console({
+			format: format.combine(
+				format.colorize(),
+				format.simple(),
+				format.printf(
+					info => `${info.timestamp} ${info.level}: ${info.message}`
+				)
+			)
+		})
+	);
+} else {
+	//prevent winston to stop if Error occurred
 	logger.exitOnError = false;
 	// logger.cli();
+	logger.exitOnError = false;
 }
 
-// // TEST all logs 
+// // TEST all logs
 // ********************
 //                       Example
 // logger.log('silly', "127.0.0.1 - Logger is working ");
@@ -86,11 +97,11 @@ if (process.env.NODE_ENV === 'production') {
 // logger.debug('debug', "127.0.0.1 - Logger is working ");
 // logger.verbose('verbose', "127.0.0.1 - Logger is working ");
 // logger.info('info', "127.0.0.1 - Logger is working info");
-// logger.warning('warn', "127.0.0.1 - Logger is working warn");
+// logger.warn('warn', "127.0.0.1 - Logger is working warn");
 // logger.error('error', "127.0.0.1 - Logger is working error");
 
 // logger.info("127.0.0.1 - Logger is working info");
-// logger.warning("127.0.0.1 - Logger is working warn");
+// logger.warn("127.0.0.1 - Logger is working warn");
 // logger.error("127.0.0.1 - Logger is working error");
 // ********************
 // var complexObject = {
@@ -105,7 +116,7 @@ if (process.env.NODE_ENV === 'production') {
 //     ]
 // }
 // test complex obj
-// logger.warning('Logger is working error', complexObject);
+// logger.warn('Logger is working error', complexObject);
 
 // logger.error('BRAND SYNC FAILED' , complexObject);
 
