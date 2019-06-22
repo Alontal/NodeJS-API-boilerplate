4d9bb1996e4df819088b498f18c9c3c47e08f221
omer <omer2500@gmail.com>
2019-06-22 20:07:30 +0300

    update new logger and fixed bug on filename

--- app/util/logger.js
+++ app/util/logger.js
@@ -1,112 +1,143 @@
-/* eslint-disable no-console */
-const winston = require('winston'),
+const { createLogger, format, transports } = require('winston'),
 	path = require('path'),
 	fs = require('fs'),
 	mkdirp = require('mkdirp'),
-	moment = require('moment');
-
-const logs_config = {
-	log_file_name: process.env.APP_DOMAIN || 'APP_DOMAIN',
-	log_file_name_Exceptions: '/uncaughtExceptions',
-	logging_dir: 'node',
-	//set the levels that will be printed in matching file
-	level_log: process.env.ENV === 'production' ? 'info' : 'debug',
-	level_exception: 'debug',
-	level_console: process.env.ENV === 'production' ? 'info' : 'debug',
-	level_rotate: process.env.ENV === 'production' ? 'info' : 'debug'
-};
+	{ SHORT_DATE } = require('../../config/config').DATE_FORMATS;
+require('winston-daily-rotate-file');
 
 const LOG_DIR = path.resolve(__dirname, '..', '..', 'LOG/');
 if (!fs.existsSync(LOG_DIR)) {
-	mkdirp(LOG_DIR, (err) => {
+	mkdirp(LOG_DIR, err => {
 		if (err) console.error(err);
 		else console.log('log directory created!');
 	});
 }
 
-console.info('Logs will write to Directory >> ', LOG_DIR);
-	
-const timeStampFormat = () => moment().format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
-
 //daily rotate log files
-require('winston-daily-rotate-file');
-const daily_rotate_transport = new(winston.transports.DailyRotateFile)({
-	filename: `${LOG_DIR}/ ${logs_config.log_file_name}.log`,
+const daily_rotate_transport = level => ({
+	filename: `${LOG_DIR}/${process.env.APP_DOMAIN ||
+		'APP_DOMAIN'}-${level}.%DATE%.log`,
 	prepend: true,
-	// level: process.env.ENV === 'development' ? 'debug' : 'info'
-	level: logs_config.level_rotate,
 	maxSize: '20m',
-	maxFiles: '14d',
-	json: true,
-	timestamp: timeStampFormat
+	maxFiles: '14d'
 });
 
-//log error messages to console and everything rate as info and below save to file (which includes error messages):
-const console_opts = { //set the level to print to console 
-	level: logs_config.level_console,
-	colorize: true,
-	prettyPrint: true,
-	timestamp: timeStampFormat,
-	json: false
-	// logstash: true
-};
+console.info('Logs will write to Directory >> ', LOG_DIR);
 
-//set winston transports to obj's define above
-var logger = new winston.Logger({
-	levels: winston.config.syslog.levels,
+const logger = createLogger({
+	level: 'info',
+	format: format.combine(
+		format.timestamp({
+			format: SHORT_DATE
+		}),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.json()
+	),
 	transports: [
-		new winston.transports.Console(console_opts),
-		// new winston.transports.File(file_opts),
-		daily_rotate_transport
-
+		//
+		// - Write to all logs with level `info` and below to `combined.log`
+		// - Write all logs error (and below) to `error.log`.
+		//
+		new transports.DailyRotateFile({
+			...daily_rotate_transport('error'),
+			level: 'error'
+		}),
+		new transports.DailyRotateFile(daily_rotate_transport('info'))
 	]
 });
-// make winston catch Exceptions and print them to diff log file
-if (process.env.NODE_ENV === 'production') {
-	logger.handleExceptions(
-		new winston.transports.File({
-			filename: LOG_DIR + logs_config.log_file_name_Exceptions
-		}));
-	//prevent winston to stop if Error occurred  
+
+// Call exceptions.handle with a transport to handle exceptions
+logger.exceptions.handle(
+	new transports.File(daily_rotate_transport('exceptions'))
+);
+//
+// If we're not in production then *ALSO* log to the `console`
+// with the colorized simple format.
+//
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(
+		new transports.Console({
+			format: format.combine(
+				format.json(),
+				format.colorize(),
+				format.simple(),
+				format.printf(
+					info =>
+						`${info.timestamp}  ${info.level}: ${info.message} ${info.data ||
+							info.stack ||
+							''}`
+				)
+			)
+		})
+	);
+} else {
+	//prevent winston to stop if Error occurred
 	logger.exitOnError = false;
-	// logger.cli();
 }
 
-// // TEST all logs 
-// ********************
-//                       Example
-// logger.log('silly', "127.0.0.1 - Logger is working ");
-// logger.log('debug', "127.0.0.1 - Logger is working ");
-// logger.log('verbose', "127.0.0.1 - Logger is working ");
-// logger.log('info', "127.0.0.1 - Logger is working info");
-// logger.log('warn', "127.0.0.1 - Logger is working warn");
-// logger.log('error', "127.0.0.1 - Logger is working error");
+// *****
+// Allows for JSON logging
+// *****
 
-// logger.silly('silly', "127.0.0.1 - Logger is working ");
-// logger.debug('debug', "127.0.0.1 - Logger is working ");
-// logger.verbose('verbose', "127.0.0.1 - Logger is working ");
-// logger.info('info', "127.0.0.1 - Logger is working info");
-// logger.warning('warn', "127.0.0.1 - Logger is working warn");
-// logger.error('error', "127.0.0.1 - Logger is working error");
+// logger.log({
+// 	level: 'info',
+// 	message: 'Pass an object and this works',
+// 	additional: 'properties',
+// 	are: 'passed along'
+//   });
 
-// logger.info("127.0.0.1 - Logger is working info");
-// logger.warning("127.0.0.1 - Logger is working warn");
-// logger.error("127.0.0.1 - Logger is working error");
-// ********************
-// var complexObject = {
-//     name: {
-//         first: "Barry",
-//         last: "Allen"
-//     },
-//     employer: "Central City Police Department",
-//     country: "United States",
-//     skills: [
-//         'The Fastest Man Alive'
-//     ]
-// }
-// test complex obj
-// logger.warning('Logger is working error', complexObject);
+//   logger.info({
+// 	message: 'Use a helper method if you want',
+// 	additional: 'properties',
+// 	are: 'passed along'
+//   });
 
-// logger.error('BRAND SYNC FAILED' , complexObject);
+//   // *****
+//   // Allows for parameter-based logging
+//   // *****
+
+//   logger.log('info', 'Pass a message and this works', {
+// 	additional: 'properties',
+// 	are: 'passed along'
+//   });
+
+//   logger.info('Use a helper method if you want', {
+// 	additional: 'properties',
+// 	are: 'passed along'
+//   });
+
+//   // *****
+//   // Allows for string interpolation
+//   // *****
+
+//   // info: test message my string {}
+//   logger.log('info', 'test message %s', 'my string');
+
+//   // info: test message my 123 {}
+//   logger.log('info', 'test message %d', 123);
+
+//   // info: test message first second {number: 123}
+//   logger.log('info', 'test message %s, %s', 'first', 'second', { number: 123 });
+
+//   // prints "Found error at %s"
+//   logger.info('Found %s at %s', 'error', new Date());
+//   logger.info('Found %s at %s', 'error', new Error('chill winston'));
+//   logger.info('Found %s at %s', 'error', /WUT/);
+//   logger.info('Found %s at %s', 'error', true);
+//   logger.info('Found %s at %s', 'error', 100.00);
+//   logger.info('Found %s at %s', 'error', ['1, 2, 3']);
+
+//   // *****
+//   // Allows for logging Error instances
+//   // *****
+
+//   logger.warn(new Error('Error passed as info'));
+//   logger.log('error', new Error('Error passed as message'));
+
+//   logger.warn('Maybe important error: ', new Error('Error passed as meta'));
+//   logger.log('error', 'Important error: ', new Error('Error passed as meta'));
+
+//   logger.error(new Error('Error as info'));
 
 module.exports = logger;
