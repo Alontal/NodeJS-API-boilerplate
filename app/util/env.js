b0692a316a0e3baf8c9ce4bc2f5dbb136a8bf3a7
omer <omer2500@gmail.com>
2019-06-27 20:30:48 +0300

    deleted chalk pacakge

--- app/util/env.js
+++ app/util/env.js
@@ -1,26 +1,22 @@
-const dotenv = require('dotenv');
-const { logger } = require('.');
-const chalk = require('chalk');
-const yellowBgRed = chalk.yellow.bgRed.bold;
-const path = require('path'),
-	fs = require('fs');
+const dotenv = require("dotenv");
+const { logger } = require(".");
+const path = require("path"),
+  fs = require("fs");
 
 // check what env needed to start app.
 logger.warn(
-	yellowBgRed(
-		`loading settings for ${process.env.NODE_ENV ||
-			'demo - (NO ENV FILE FOUND!, please create .env.debug && .env.production. (see .env.demo for example)'} `
-	)
+  `loading settings for ${process.env.NODE_ENV ||
+    "demo - (NO ENV FILE FOUND!, please create .env.debug && .env.production. (see .env.demo for example)"} `
 );
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'debug') {
-	// check if there is no '.debug.env' file then use demo one
-	// demo file DO NOT includes in .gitignore therefore it must be replaced !
-	const ENV_PATH = path.resolve(__dirname, '..', '..', '.env.debug');
-	const isEnv = fs.existsSync(ENV_PATH);
-	let env = dotenv.config({
-		path: isEnv ? ENV_PATH : path.resolve(__dirname, '..', '..', '.env.demo')
-	}); //load debug env
-	if (env.error) {
-		throw Error(env.error);
-	}
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "debug") {
+  // check if there is no '.debug.env' file then use demo one
+  // demo file DO NOT includes in .gitignore therefore it must be replaced !
+  const ENV_PATH = path.resolve(__dirname, "..", "..", ".env.debug");
+  const isEnv = fs.existsSync(ENV_PATH);
+  let env = dotenv.config({
+    path: isEnv ? ENV_PATH : path.resolve(__dirname, "..", "..", ".env.demo")
+  }); //load debug env
+  if (env.error) {
+    throw Error(env.error);
+  }
 }
--- app/util/errorHandle.js
+++ app/util/errorHandle.js
@@ -1,37 +1,38 @@
-const {logger} = require('.'),
-	chalk =require('chalk'),
-	E = module.exports;
+const { logger } = require("."),
+  E = module.exports;
 
 E.Handle = (message, error, source) => {
-	// console.log('err :', err);
-	// console.log('ERROR in '+source , err );
-	logger.error(chalk.red.bgBlack.bold(`${message} @ ${source}`), { Error: error });
-	return null;
+  // console.log('err :', err);
+  // console.log('ERROR in '+source , err );
+  logger.error(`${message} @ ${source}`, {
+    Error: error
+  });
+  return null;
 };
 
 //catch ERRORS
-process.on('uncaughtException', function(err) {
-	logger.error(chalk.red.bgBlack.bold('uncaughtException'), err);
-	closeProcess();
+process.on("uncaughtException", function(err) {
+  logger.error("uncaughtException", err);
+  closeProcess();
 });
-process.on('unhandledRejection', (reason, p) => {
-	logger.info(chalk.yellow.bgBlue.bold('unhandledRejection ' + reason + ' p: ' + p));
-	logger.info(chalk.yellow.bgBlue.bold('reason.stack '), reason.stack || reason);
-	closeProcess();
-});
-
-process.on('warning', warning => {
-	logger.error(chalk.yellow.bgBlue.bold('warning :', warning.name + ' : ' + warning.message));
+process.on("unhandledRejection", (reason, p) => {
+  logger.info("unhandledRejection " + reason + " p: " + p);
+  logger.info("reason.stack ", reason.stack || reason);
+  closeProcess();
 });
 
-process.on('exit', warning => {
-	logger.error(chalk.yellow.bgBlue.bold('warning :', warning.name + ' : ' + warning.message));
-	// TODO: send email notify system crashed with error and stack
+process.on("warning", warning => {
+  logger.error("warning :", warning.name + " : " + warning.message);
+});
+
+process.on("exit", warning => {
+  logger.error("warning :", warning.name + " : " + warning.message);
+  // TODO: send email notify system crashed with error and stack
 });
 
 function closeProcess() {
-	setTimeout(function() {
-		logger.error(chalk.red.bgBlack.bold('Closing Process'));
-		process.env.NODE_ENV === 'production' ? process.exit(1) : {};
-	}, 2000);
+  setTimeout(function() {
+    logger.error("Closing Process");
+    process.env.NODE_ENV === "production" ? process.exit(1) : {};
+  }, 2000);
 }
