0659524db6317b2d2a595dde2868657db4c445a5
omer2500 <omer2500@gmail.com>
2019-10-17 19:36:00 +0300

    updated the index file for user component

--- app/components/user/index.js
+++ app/components/user/index.js
@@ -1,5 +1,2 @@
-module.exports.userSchema = require('./userSchema');
-module.exports.userModel = require('./userModel');
-module.exports.userController = require('./userController');
-module.exports.userValidator = require('./userValidator');
+module.exports.userService = require('./userService');
 module.exports.userApi = require('./userApi');
