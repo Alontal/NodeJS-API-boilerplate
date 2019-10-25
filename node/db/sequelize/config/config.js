de5e91e14ae573c9be0d9027a99a22855dfd6fc4
alon tal <alon@digitplatinum.com>
2019-10-25 21:02:40 +0300

    style db config

--- node/db/sequelize/config/config.js
+++ node/db/sequelize/config/config.js
@@ -1,25 +1,41 @@
-require('dotenv').config();
+const {
+  LOCAL_DB_USERNAME,
+  LOCAL_DB_PASSWORD,
+  LOCAL_DB_DATABASE,
+  LOCAL_DB_HOST,
+  LOCAL_DB_DIALECT,
+  DEVELOPMENT_DB_USERNAME,
+  DEVELOPMENT_DB_PASSWORD,
+  DEVELOPMENT_DB_DATABASE,
+  DEVELOPMENT_DB_HOST,
+  DEVELOPMENT_DB_DIALECT,
+  PRODUCTION_DB_USERNAME,
+  PRODUCTION_DB_PASSWORD,
+  PRODUCTION_DB_DATABASE,
+  PRODUCTION_DB_HOST,
+  PRODUCTION_DB_DIALECT
+} = process.env;
 
 module.exports = {
   local: {
-    username: process.env.TEST_DB_USERNAME,
-    password: process.env.TEST_DB_PASSWORD,
-    database: process.env.TEST_DB_DATABASE,
-    host: process.env.TEST_DB_HOST,
-    dialect: 'mysql'
+    username: LOCAL_DB_USERNAME,
+    password: LOCAL_DB_PASSWORD,
+    database: LOCAL_DB_DATABASE,
+    host: LOCAL_DB_HOST,
+    dialect: LOCAL_DB_DIALECT
   },
   development: {
-    username: process.env.DEVELOPMENT_DB_USERNAME,
-    password: process.env.DEVELOPMENT_DB_PASSWORD,
-    database: process.env.DEVELOPMENT_DB_DATABASE,
-    host: process.env.DEVELOPMENT_DB_HOST,
-    dialect: 'mysql'
+    username: DEVELOPMENT_DB_USERNAME,
+    password: DEVELOPMENT_DB_PASSWORD,
+    database: DEVELOPMENT_DB_DATABASE,
+    host: DEVELOPMENT_DB_HOST,
+    dialect: DEVELOPMENT_DB_DIALECT
   },
   production: {
-    username: process.env.PRODUCTION_DB_USERNAME,
-    password: process.env.PRODUCTION_DB_PASSWORD,
-    database: process.env.PRODUCTION_DB_DATABASE,
-    host: process.env.PRODUCTION_DB_HOST,
-    dialect: 'mysql'
+    username: PRODUCTION_DB_USERNAME,
+    password: PRODUCTION_DB_PASSWORD,
+    database: PRODUCTION_DB_DATABASE,
+    host: PRODUCTION_DB_HOST,
+    dialect: PRODUCTION_DB_DIALECT
   }
 };
