add3e12e56fe926e63f97f81727f2faf56480262
alon tal <alon@digitplatinum.com>
2019-10-26 04:41:39 +0300

    move .env to root folder - set dotenv path to match

--- node/server.js
+++ node/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: `${__dirname}/../.env` });
 
 const app = express();
 const helmet = require('helmet');
