d8ff4d89b976b231844c47665b385b541cd31dfb
omer <omer2500@gmail.com>
2019-10-26 14:53:37 +0300

    added a connection test to sql db

--- node/server.js
+++ node/server.js
@@ -9,6 +9,8 @@ const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { LUSCA_OPTIONS, APP_SESSION, RATE_LIMITER } = require('./config/config');
+const { sequelize } = require('./db/sequelize/models');
+
 // show nice console text
 const text = require('./loadingCliText');
 /* eslint-disable no-console */
@@ -18,6 +20,7 @@ const { logger } = require('./app/util');
 
 // eslint-disable-next-line no-undef
 const { PORT } = process.env;
+
 // set helmet for security
 app.use(helmet());
 // Parse req made to app
@@ -55,3 +58,13 @@ require('./app/bootstrap');
 
 // start our server on port set in .env file
 app.listen(PORT, () => logger.info(`Server started running... on port: ${PORT}`));
+
+// Check connection to Database
+sequelize
+  .authenticate()
+  .then(() => {
+    return logger.info(`Connected to database`);
+  })
+  .catch(err => {
+    logger.error('Failed to connect to the database:', err);
+  });
